119f2dfe2045af93b737d72a90747909750b9ee8
Özhan Gebeşoğlu <ozhan.gebesoglu@gmail.com>
2025-06-28 22:30:20 +0000

    böyle dedi bu seferde

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBMDoglKQ_qbCO1s0DeUm_J2jTFOQ5DeFA",
-  authDomain: "merhabaapp-14bd8.firebaseapp.com",
-  projectId: "merhabaapp-14bd8",
-  storageBucket: "merhabaapp-14bd8.appspot.com",
-  messagingSenderId: "432273595838",
-  appId: "1:432273595838:web:8e530c3808142ba533edef"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
